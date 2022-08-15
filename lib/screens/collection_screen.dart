816a62b647a18cca79ff80a03700990df7a7ac6c
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-08-15 15:13:22 -0500

    working on collections

--- lib/screens/collection_screen.dart
+++ lib/screens/collection_screen.dart
@@ -10,10 +10,52 @@ class CollectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppAppBar('Collections', context),
-      body: Column(
-        children: [],
+      body: ListView(
+        padding: EdgeInsets.all(15),
+        children: [
+          buildCategory(Colors.green, 'Children'),
+          SizedBox(height: 30),
+          buildCategory(Colors.orange, 'Men'),
+          SizedBox(height: 30),
+          buildCategory(Colors.indigo, 'Women'),
+        ],
       ),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
+}
+
+Widget buildCategory(Color containerColor, String text) {
+  return Container(
+    padding: EdgeInsets.all(8),
+    height: 250,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color: containerColor,
+    ),
+    child: Stack(
+      children: [
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Positioned(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.black.withAlpha(100),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            bottom: 15,
+          ),
+        )
+      ],
+    ),
+  );
 }
