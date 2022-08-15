7a0f7c9d732870495c5e8bf0e6efe5e6fdc09b09
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-08-15 12:11:26 -0500

    working on an ecommerce app

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:ecommerce/screens/collection_screen.dart';
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CollectionScreen(),
+      theme: ThemeData(
+        fontFamily: 'Josefin',
+        appBarTheme: AppBarTheme.of(context).copyWith(
+          elevation: 0,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          onPrimary: Colors.white,
+          padding: EdgeInsets.all(15),
+        )),
+        primaryColor: Colors.indigo,
+        colorScheme: ColorScheme(
+          background: Colors.pink,
+          primary: Colors.white,
+          secondary: Colors.blue,
+          surface: Colors.purpleAccent,
+          error: Colors.red,
+          onPrimary: Colors.black,
+          onSecondary: Colors.white,
+          onSurface: Colors.teal,
+          onBackground: Colors.blue,
+          onError: Colors.redAccent,
+          brightness: Brightness.light,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/collection_screen.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
+
+class CollectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Collection',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: Icon(Icons.apps),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart_outlined),
+            onPressed: () {},
+          ),
+          SizedBox(
+            width: 15,
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Crazy ass button'),
+          )
+        ],
+      ),
+    );
+  }
+}
